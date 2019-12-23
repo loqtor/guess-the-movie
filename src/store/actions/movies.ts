54bdc572c60ebe5d1c397a51376d527e43debd00
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-12-24 10:47:28 +1300

    Added actions for success and failure when calling the API.

--- src/store/actions/movies.ts
+++ src/store/actions/movies.ts
@@ -1,7 +1,11 @@
 export const GET_MOVIES = 'GET_MOVIES';
+export const GET_MOVIES_SUCCESS = 'GET_MOVIES_SUCCESS';
+export const GET_MOVIES_FAIL = 'GET_MOVIES_FAIL';
 
 export const MOVIES_TYPES = {
   GET_MOVIES,
+  GET_MOVIES_SUCCESS,
+  GET_MOVIES_FAIL,
 };
 
 export const getMovies = () => {
--- src/store/reducers/movies.ts
+++ src/store/reducers/movies.ts
@@ -6,18 +6,32 @@ export type Movie = {
 
 export type MoviesState = {
   movies: Movie[];
+  loadingMovies: boolean;
 }
 
 const initialState: MoviesState = {
   movies: [],
+  loadingMovies: false,
 };
 
-export const movies = (state = initialState, action: { payload: { movies: Movie[] }, type: any }) => {
+export const movies = (state = initialState, action: { payload: { movies: Movie[], error: string }, type: any }) => {
   switch (action.type) {
     case MOVIES_TYPES.GET_MOVIES: 
       return {
         ...state,
+        loadingMovies: true,
+      };
+    case MOVIES_TYPES.GET_MOVIES_SUCCESS:
+      return {
+        ...state,
+        loadingMovies: false,
         movies: action.payload.movies,
+      };
+    case MOVIES_TYPES.GET_MOVIES_FAIL:
+      return {
+        ...state,
+        loadingMovies: false,
+        error: action.payload.error,
       };
 
     default:
