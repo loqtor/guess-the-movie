0bb6dee40164f65330028295c82716d749aeb183
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-12-26 09:30:16 +1300

    Completed `PhotoCropper` logic. Added a DEFAULT_IMAGE_SIZE to crop.

--- src/components/game/PhotoCropper.tsx
+++ src/components/game/PhotoCropper.tsx
@@ -1,3 +1,49 @@
 import React from 'react';
+import { generateRandomNumberFromRange } from '../../tools/util';
 
-export const PhotoCropper = () => (<h1>This is the PhotoCropper</h1>);
+interface IProps {
+  imageUrl: string;
+  imagePositionX?: number;
+  imagePositionY?: number;
+  expectedImageWidth?: number;
+}
+
+const POSITION_BOUNDARIES_X = {
+  max: 25,
+  min: 75,
+};
+
+const POSITION_BOUNDARIES_Y = {
+  max: 30,
+  min: 60,
+};
+
+const DEFAULT_IMAGE_WIDTH = 1000;
+
+export const PhotoCropper = (props: IProps) => {
+  const { imageUrl, imagePositionX, imagePositionY, expectedImageWidth } = props;
+  const imagePosition = setImagePosition(imagePositionX, imagePositionY);
+  const finalImageWidth = expectedImageWidth || DEFAULT_IMAGE_WIDTH;
+
+  const imageStyles = {
+    maxWidth: `${finalImageWidth / 2}px`, // ensures it's cropped
+    objectFit: 'none',
+    objectPosition: imagePosition,
+  } as React.CSSProperties;
+  
+  return (
+    <img style={imageStyles} src={imageUrl}/>
+  );
+};
+
+const setImagePosition = (positionX?: number, positionY?: number) => {
+  const positionXFinal = positionX || positionX === 0 ?
+    positionX:
+    generateRandomNumberFromRange(POSITION_BOUNDARIES_X.max, POSITION_BOUNDARIES_X.min);
+
+  const positionYFinal = positionY || positionY === 0 ?
+    positionY:
+    generateRandomNumberFromRange(POSITION_BOUNDARIES_Y.max, POSITION_BOUNDARIES_Y.min);
+
+  return `${positionXFinal}% ${positionYFinal}%`;
+}
