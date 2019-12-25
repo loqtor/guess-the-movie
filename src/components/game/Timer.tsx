9b860bc6067368dd2909af0d60ac0addd4017a2a
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-12-26 08:33:26 +1300

    Created timer component.

--- src/components/game/Timer.tsx
+++ src/components/game/Timer.tsx
@@ -1,3 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const Timer = () => (<h1>This is the timer</h1>);
+interface IProps {
+  time: number;
+  onTimeUp: () => void;
+}
+
+interface IState {
+  timeLeft: number;
+}
+
+export const Timer = class Timer extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      timeLeft: props.time,
+    };
+  }
+
+  tickTimer = () => {
+    const { timeLeft } = this.state;
+
+    this.setState({
+      timeLeft: timeLeft - 1,
+    }, () => {
+      setTimeout(() => {
+        if (timeLeft > 1) {
+          this.tickTimer();
+        } else {
+          const { onTimeUp } = this.props;
+
+          onTimeUp();
+        }
+      }, 1000);
+    });
+  }
+
+  componentDidMount() {
+    /**
+     * Otherwise the start time would not be seen
+     * before the game starts (i.e.: if the time is 60 the user would
+     * only see from 59 ahead)
+     */
+    setTimeout(() => {
+      this.tickTimer();
+    }, 1000);
+  }
+
+  render() {
+    const { timeLeft } = this.state;
+
+    return (<p>{timeLeft}</p>);
+  }
+}
