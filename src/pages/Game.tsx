3e7f1300c3f814239fd75c62378352e6975d1e82
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-12-26 09:26:55 +1300

    Changed to backdrop image for now to avoid titles showing up.

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -12,6 +12,7 @@ import { Movie } from '../store/reducers/movies';
 import { IMAGE_BASE_URL } from "../constants/config";
 import { Timer } from '../components/game/Timer';
 import { GameStatus } from '../constants/game';
+import { PhotoCropper } from '../components/game/PhotoCropper';
 
 export interface OwnProps {}
 
@@ -75,15 +76,14 @@ class GameComponent extends React.Component<Props, OwnStateProps> {
 
     return (
       <>
-        <Timer time={10} onTimeUp={this.finishGame} />
+        <Timer time={10000} onTimeUp={this.finishGame} />
         <h1>This is where the game will happen.</h1>
         <p>This is where we'll show the movies</p>
         {movies && movies.length > 0 && (
           movies.map((movie: Movie) => (
             <>
               <p key={`title-${movie.id}`}>{movie.title}</p>
-              <img key={`image-${movie.id}`} style={{maxWidth: '200px'}} src={`${IMAGE_BASE_URL}${movie.poster_path}`}/>
-              <img key={`backdrop-${movie.id}`} style={{maxWidth: '200px'}} src={`${IMAGE_BASE_URL}${movie.backdrop_path}`}/>
+              <PhotoCropper key={`image-${movie.id}`} imageUrl={`${IMAGE_BASE_URL}${movie.backdrop_path}`}/>
             </>
           ))
         )}
