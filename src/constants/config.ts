b0087449adf7deedd3071bb5b14949c84c9d48be
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-12-26 08:12:32 +1300

    Testing showing the images from the API. Added /constants/config for those deets.

---
+++ src/constants/config.ts
@@ -0,0 +1,2 @@
+// Base URL for the images from the API
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -9,6 +9,8 @@ import { getMovies as getMoviesAction } from "../store/actions/movies";
 import { RootState } from '../store/reducers';
 import { Movie } from '../store/reducers/movies';
 
+import { IMAGE_BASE_URL } from "../constants/config";
+
 export interface OwnProps {}
 
 interface StateProps {
@@ -50,7 +52,13 @@ class GameComponent extends React.Component<Props, {}> {
         <h1>This is where the game will happen.</h1>
         <p>This is where we'll show the movies</p>
         {movies && movies.length > 0 && (
-          movies.map((movie: Movie) => (<p key={movie.id}>{movie.title}</p>))
+          movies.map((movie: Movie) => (
+            <>
+              <p key={`title-${movie.id}`}>{movie.title}</p>
+              <img key={`image-${movie.id}`} style={{maxWidth: '200px'}} src={`${IMAGE_BASE_URL}${movie.poster_path}`}/>
+              <img key={`backdrop-${movie.id}`} style={{maxWidth: '200px'}} src={`${IMAGE_BASE_URL}${movie.backdrop_path}`}/>
+            </>
+          ))
         )}
         
       </>
