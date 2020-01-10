ff67a75e4143a100336f7cb61890d89c4072addd
Martin Martinez <martin@alphero.com>
2020-01-11 08:08:57 +1300

    Updated Game time to 3 minutes.

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 // Base URL for the images from the API
 export const MOVIES_PER_GAME = 10;
-export const GAME_TIME = 120;
+export const GAME_TIME = 180;
 export const IMAGE_WIDTH = 780;
 export const THUMBNAIL_WIDTH = 60;
 export const IMAGE_BASE_URL = `https://image.tmdb.org/t/p/w${IMAGE_WIDTH}`;
